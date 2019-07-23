d6a1abae73ad80d95deff48a1d57ce07c3e9b818
Elston Corda <elston.corda98@gmail.com>
2019-07-23 19:13:48 +0530

    Changes made to the loading icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,7 @@
 particlesJS.load('particles-js', 'assets/js/particlesjs-config.json', function() {
     console.log('callback - particles.js config loaded');
 });
+
+particlesJS.load('particles-js2', 'assets/js/particlesjssecond-config.json', function() {
+    console.log('callback - particles2.js config loaded');
+})
